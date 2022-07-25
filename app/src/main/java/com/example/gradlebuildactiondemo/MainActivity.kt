b7c76c81a83b64d8631c7c024e0ac22d66c8a64b
Masatoshi Takada <masatoshi.takada@zozo.com>
2022-07-25 15:47:53 +0900

    add log

--- app/src/main/java/com/example/gradlebuildactiondemo/MainActivity.kt
+++ app/src/main/java/com/example/gradlebuildactiondemo/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.gradlebuildactiondemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,5 +10,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         print("hello from develop update")
+        Log.d("MAINCACITIVITY_LOG", "Any msg from log")
     }
 }
