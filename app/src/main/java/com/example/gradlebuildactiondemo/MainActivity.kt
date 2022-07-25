4e51824f75da3f56a3547346ed3f473bf751f448
Masatoshi Takada <masatoshi.takada@zozo.com>
2022-07-25 15:53:22 +0900

    change to toast

--- app/src/main/java/com/example/gradlebuildactiondemo/MainActivity.kt
+++ app/src/main/java/com/example/gradlebuildactiondemo/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.gradlebuildactiondemo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,6 +11,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         print("hello from develop update")
-        Log.d("MAINCACITIVITY_LOG", "Any msg from log")
+        Toast.makeText(applicationContext, "Any msg from log", Toast.LENGTH_SHORT).show()
     }
 }
